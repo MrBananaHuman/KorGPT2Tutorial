05a7e2eb40237075556fdb288f86c6a1b7737ec0
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-06-11 14:49:41 +0900

    Create generation_text.py

---
+++ generation_text.py
@@ -0,0 +1,33 @@
+from transformers import GPT2LMHeadModel, GPT2Config
+from new_tokenizer import MyTokenizer
+import torch
+
+vocab_file_path = '../tokenizer/vocab.json'
+merge_file_path = '../tokenizer/merges.txt'
+
+tokenizer = MyTokenizer(vocab_file_path, merge_file_path)
+bos = tokenizer.convert_tokens_to_ids('<s>')
+eos = tokenizer.convert_tokens_to_ids('</s>')
+pad = tokenizer.convert_tokens_to_ids('<pad>')
+
+config = GPT2Config(vocab_size=52000, resid_pdrop=0, embd_pdrop=0, attn_pdrop=0, summary_first_dropout=0)
+model = GPT2LMHeadModel(config)
+
+model_dir = '../KorGPT-2SampleModel/pytorch_model.bin'
+
+model.load_state_dict(torch.load(model_dir), strict=False)
+model.to('cpu')
+
+def encoding(text):
+    tokens = ['<s>'] + tokenizer.tokenize(text)
+    return torch.tensor(tokenizer.convert_tokens_to_ids(tokens)).unsqueeze(0)
+
+def decoding(ids):
+    return tokenizer.convert_ids_to_tokens(ids[0])
+
+input_ids = encoding('오늘도 좋은 하루를')
+greedy_output = model.generate(input_ids, max_length=20, bos_token_id=bos, pad_token_id=pad, eos_token_id=eos)
+
+print(decoding(greedy_output.tolist()))
+
+
