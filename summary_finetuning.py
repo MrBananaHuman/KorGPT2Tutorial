7b3f0b682240382930571360864e029a592aeeb6
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-06-15 18:11:17 +0900

    Update summary_finetuning.py

--- summary_finetuning.py
+++ summary_finetuning.py
@@ -105,4 +105,4 @@ for epoch in range(epochs):
 			print('epoch no.{0} train no.{1}  loss = {2:.5f} avg_loss = {3:.5f}' . format(epoch, count, loss, avg_loss[0] / avg_loss[1]))
 		count += 1
 
-torch.save(model.state_dict(), '/'.join(model_dir.split('/')[:-1]) + '/lyric_model.bin' )
+torch.save(model.state_dict(), '/'.join(model_dir.split('/')[:-1]) + '/summary_model.bin' )
