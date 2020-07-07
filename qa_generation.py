5d956b598cd36ec3f44557b9c1ca79f8a2e8a14f
MrBananaHuman <34882690+MrBananaHuman@users.noreply.github.com>
2020-07-07 17:43:19 +0900

    Update qa_generation.py

--- qa_generation.py
+++ qa_generation.py
@@ -45,7 +45,7 @@ def context_tokenizer(text, tokenizer):
     sent_list = kss.split_sentences(text)
     tokens = []
     for sent in sent_list:
-        tokenized_sentence = tokenizer.tokenize(text)
+        tokenized_sentence = tokenizer.tokenize(sent)
         if len(tokens) + len(tokenized_sentence) < 912:
             tokens += ['<s>'] + tokenized_sentence + ['</s>']
         else:
